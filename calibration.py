b96b8ff7ec91262061196b568630e34f292efa4d
kaetze66 <kaetze66@gmail.com>
2020-04-04 11:22:43 +0200

    added contact matrix for switzerland

--- calibration.py
+++ calibration.py
@@ -1,3 +1,6 @@
 # script for calibration to a countries data
 # make sure the country data is loaded
 
+def calibrate(model):
+    def loss_function(model,param_dict):
+        res = model.run(params=param_dict,return_columns=[''])
